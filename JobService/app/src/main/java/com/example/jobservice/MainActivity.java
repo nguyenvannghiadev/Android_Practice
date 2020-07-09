cf6103aa2b8a746f7009704030e4b268dfad24da
Nguyen Van Nghia <nghia.nguyen@tp.com.vn>
2020-07-09 11:06:05 +0700

    Research JobService continue

--- JobService/app/src/main/java/com/example/jobservice/MainActivity.java
+++ JobService/app/src/main/java/com/example/jobservice/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.jobservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.jobservice.service.TestJobService;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+	private static final String TAG = "MainActivity";
+	private static final int JOB_ID = 123;
+	private Button btnStartJob, btnCanceJob;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnStartJob = findViewById(R.id.start_job);
+		btnCanceJob = findViewById(R.id.cance_job);
+		btnStartJob.setOnClickListener(this);
+		btnCanceJob.setOnClickListener(this);
+	}
+
+	public void scheduleJob(View view) {
+		ComponentName componentName = new ComponentName(this, TestJobService.class);
+		JobInfo info = new JobInfo.Builder(JOB_ID, componentName)
+				.setRequiresCharging(true)
+				.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+				.setPersisted(true)
+				.setMinimumLatency(1000)
+				.setOverrideDeadline(2*1000)
+				.build();
+		JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+		int resultCode = scheduler.schedule(info);
+		if (resultCode == JobScheduler.RESULT_SUCCESS) {
+			Log.d(TAG, "Job scheduled");
+		} else {
+			Log.d(TAG, "Job scheduling failed");
+		}
+	}
+
+	public void canceJob(View view) {
+		JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+		scheduler.cancel(JOB_ID);
+		Log.d(TAG, " Job cancelled");
+	}
+
+	@Override
+	public void onClick(View v) {
+		int vId = v.getId();
+		if (vId == R.id.start_job) {
+			scheduleJob(v);
+		} else if (vId == R.id.cance_job){
+			canceJob(v);
+		}
 	}
 }
--- JobService/app/src/main/java/com/example/jobservice/service/TestJobService.java
+++ JobService/app/src/main/java/com/example/jobservice/service/TestJobService.java
@@ -2,15 +2,44 @@ package com.example.jobservice.service;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.util.Log;
+import android.widget.Toast;
 
 public class TestJobService extends JobService {
+	private static final String TAG = "TestJobService";
+	private boolean jobCancelled = false;
 	@Override
 	public boolean onStartJob(JobParameters params) {
-		return false;
+		Log.d(TAG, "Job started ");
+		doBackgroundWork(params);
+		return true;
 	}
 
+	private void doBackgroundWork(final JobParameters params) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					Log.d(TAG, "run: " + i);
+					if (jobCancelled) {
+						return;
+					}
+					try {
+						Thread.sleep(1000);
+
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				Log.d(TAG, "Job finished");
+				jobFinished(params, false);
+			}
+		}).start();
+	}
 	@Override
 	public boolean onStopJob(JobParameters params) {
-		return false;
+		Log.d(TAG, "Job cancelled before completion");
+		jobCancelled = true;
+		return true;
 	}
 }
