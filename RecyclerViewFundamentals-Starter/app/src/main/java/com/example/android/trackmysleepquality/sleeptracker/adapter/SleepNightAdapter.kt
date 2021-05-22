c4593e8956660b6e28ab239a46a7bcfa363632f4
Nguyen Van Nghia <nghia.nguyen@tp.com.vn>
2021-05-22 16:01:18 +0700

    Kotlin: Recyclerview part2 _ refesh, update.. Apdapter

--- RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/adapter/SleepNightAdapter.kt
+++ RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/adapter/SleepNightAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.android.trackmysleepquality.sleeptracker.adapter
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
+import kotlinx.android.synthetic.main.list_item_sleep_night.view.*
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.TextItemViewHolder>() {
+class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     var data = listOf<SleepNight>()
         set(value) { //set & update data
             field = value
@@ -16,29 +19,54 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.TextItemViewHol
         }
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val itemBinding = DataBindingUtil.inflate<ListItemSleepNightBinding>(
+            layoutInflater,
+            R.layout.list_item_sleep_night,
+            parent,
+            false
+        )
+        return ViewHolder(itemBinding)
     }
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-        val item = data[position]
-        holder.textView.text = item.sleepQuality.toString()
-        if (item.sleepQuality <= 1) {
-            holder.textView.setTextColor(Color.RED)
-        } else {
-            holder.textView.setTextColor(Color.BLACK)
-        }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val itemView = data[position]
+        holder.onBind(itemView, position)
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
+
     // ViewHolder
-    @Suppress("UNREACHABLE_CODE")
-    class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {
+    class ViewHolder(itemView: ListItemSleepNightBinding) : RecyclerView.ViewHolder(itemView.root) {
+        private lateinit var sleepNight: SleepNight
+        private var mPosition = 0
+        fun onBind(sleepNight: SleepNight, position: Int) {
+            this.sleepNight = sleepNight
+            this.mPosition = position
+            itemView.sleep_length.text = convertDurationToFormatted(
+                sleepNight.startTimeMilli,
+                sleepNight.endTimeMilli,
+                itemView.resources
+            )
+            itemView.quality_string.text =
+                convertNumericQualityToString(sleepNight.sleepQuality, itemView.resources)
+            itemView.quality_image.setImageResource(
+                when (sleepNight.sleepQuality) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.ic_sleep_active
+                }
+            )
+
+        }
 
     }
 }
