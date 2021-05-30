c68cbeacc22162221a5dc3594c2294d1cb4d2001
Nguyen Van Nghia <nghia.nguyen@tp.com.vn>
2021-05-30 17:07:03 +0700

    Kotlin: Room. Use Room caching data offline

---
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -0,0 +1,70 @@
+package com.example.android.devbyteviewer.network
+
+import com.example.android.devbyteviewer.database.DatabaseVideo
+import com.example.android.devbyteviewer.domain.DevByteVideo
+import com.squareup.moshi.JsonClass
+
+/**
+ * DataTransferObjects go in this file. These are responsible for parsing responses from the server
+ * or formatting objects to send to the server. You should convert these to domain objects before
+ * using them.
+ *
+ * @see domain package for
+ */
+
+/**
+ * VideoHolder holds a list of Videos.
+ *
+ * This is to parse first level of our network result which looks like
+ *
+ * {
+ *   "videos": []
+ * }
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkVideoContainer(val videos: List<NetworkVideo>)
+
+/**
+ * Videos represent a devbyte that can be played.
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkVideo(
+	val title: String,
+	val description: String,
+	val url: String,
+	val updated: String,
+	val thumbnail: String,
+	val closedCaptions: String?
+)
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
+	return videos.map {
+		DevByteVideo(
+			title = it.title,
+			description = it.description,
+			url = it.url,
+			updated = it.updated,
+			thumbnail = it.thumbnail
+		)
+	}
+}
+
+/**
+ * Convert Network results to database objects
+ */
+
+fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
+	return videos.map {
+		DatabaseVideo(
+			url = it.url,
+			title = it.title,
+			description = it.description,
+			updated = it.updated,
+			thumbnail = it.thumbnail
+		)
+	}
+}
+
